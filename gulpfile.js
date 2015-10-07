47be51a2cbea23b1811109fe35b2401c2d3a2a3b
Stéphane Deluce <boluge@gmail.com>
2015-10-07 21:50:14 +0200

    init

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+
+var configcss = [
+    require('precss'),
+    require('rucksack-css'),   
+    require("postcss-color-function"),
+    require('autoprefixer')({ browsers: ['last 5 versions'] }),
+    require("css-mqpacker")(),
+    //require('cssnano')
+]
+
+gulp.task('sass', function(){
+	gulp.src('sass/prepross.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('css/'))
+})
+
+gulp.task('css', ['sass'], function(){
+	gulp.src('css/style.dev.css')
+		.pipe(postcss(configcss))
+		.pipe(rename('style.css'))
+		.pipe(gulp.dest('css/'))
+})
