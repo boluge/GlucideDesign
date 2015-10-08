1980e69d54ad8cd44ef972f85ca575bb78431861
Stephane <boluge@gmail.com>
2015-10-08 18:10:22 +0200

    up

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,57 @@
+'use strict';
+
 var gulp 		= require('gulp');
 var postcss 	= require('gulp-postcss');
 var rename 		= require('gulp-rename');
 var sourcemaps 	= require('gulp-sourcemaps');
 var sass 		= require('gulp-sass');
-var polybuild 	= require('polybuild');
+var polybuild = require('polybuild');
 var browserSync = require('browser-sync').create();
 var reload      = browserSync.reload;
 
 var configcss = [
     require('precss'),
-    require('rucksack-css'),   
+    require('rucksack-css'),
     require("postcss-color-function"),
     require('autoprefixer')({ browsers: ['last 5 versions'] }),
     require("css-mqpacker")(),
-    //require('cssnano')
+    require('cssnano')
 ]
 
+var vulcanize = {
+  inlineScripts: true,
+  inlineCss: true
+}
+
+
 gulp.task('sass', function(){
-	gulp.src('sass/prepross.scss')
+	return gulp.src('sass/prepross.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('css/'))
 })
 
 gulp.task('css', ['sass'], function(){
-	gulp.src('css/style.dev.css')
+	return gulp.src('css/style.dev.css')
 		.pipe(postcss(configcss))
 		.pipe(rename('style.css'))
 		.pipe(gulp.dest('css/'))
 })
 
 gulp.task('polymer', function(){
-	gulp.src('elements/elements.html')
-		.pipe(polybuild({ maximumCrush: true }))
+	return gulp.src('elements/elements.html')
+        .pipe(polybuild({
+            maximumCrush: false
+        }))
 		.pipe(gulp.dest('elements/'))
 })
 
-gulp.task('default', function () {
+gulp.task('default',['css'], function () {
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
-    gulp.watch("*.html").on("change", browserSync.reload);
+    gulp.watch(["sass/*.scss","css/style.dev.css"], ['css']);
+    //gulp.watch("elements/elements.html", ['polymer']);
+    gulp.watch(["css/style.css", "*.html", "elements/elements.build.html"]).on("change", browserSync.reload);
 })
